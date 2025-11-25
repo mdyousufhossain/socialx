b9fb83dfafb69ffbc211727ddcd9903896981a02
mdyousufhossain <yousafhossain3@gmail.com>
2025-11-25 23:29:06 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -11,23 +11,23 @@ const nextConfig = {
         headers: [
           {
             key: 'Access-Control-Allow-Origin',
-            value: '*',
+            value: '*'
           },
           {
             key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, OPTIONS',
+            value: 'GET, POST, OPTIONS'
           },
           {
             key: 'Access-Control-Allow-Headers',
-            value: 'Authorization, Content-Type',
-          },
-        ],
-      },
+            value: 'Authorization, Content-Type'
+          }
+        ]
+      }
     ]
   },
   images: {
-    domains: ['res.cloudinary.com', 'upload.wikimedia.org'],
-  },
+    domains: ['res.cloudinary.com', 'upload.wikimedia.org']
+  }
 }
 
 export default nextConfig
